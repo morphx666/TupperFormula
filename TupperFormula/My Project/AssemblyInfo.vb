c84bb047a248de0b28a6e7c7ef3cc791e2407f9a
Xavier Flix <morphx@gmail.com>
2016-12-16 14:04:18 -0400

    Updated DirectBItmap with some helper graphing functions

--- TupperFormula/My Project/AssemblyInfo.vb
+++ TupperFormula/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("2016.7.3.329")>
-<Assembly: AssemblyFileVersion("2016.7.3.329")>
+<Assembly: AssemblyVersion("2016.11.21.332")>
+<Assembly: AssemblyFileVersion("2016.11.21.332")>
