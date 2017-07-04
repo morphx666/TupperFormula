89ef3c0cf763c8ada819e0cf5f4622acfb31cbc2
Xavier Flix <morphx@gmail.com>
2017-07-04 03:13:33 -0400

    Fixed initial (code-base) formula assignment

--- TupperFormula/My Project/AssemblyInfo.vb
+++ TupperFormula/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("2016.11.21.332")>
-<Assembly: AssemblyFileVersion("2016.11.21.332")>
+<Assembly: AssemblyVersion("2017.7.4.348")>
+<Assembly: AssemblyFileVersion("2017.7.4.348")>
